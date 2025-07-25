77e751e9bbd169bd835aa0e7ce6278cdcc0f30f6
Hema-Nath-Reddy <164506740+Hema-Nath-Reddy@users.noreply.github.com>
2025-07-25 15:59:06 +0530

    updated nav bar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -2,9 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 const NavBar = () => {
   return (
-    <div>
-      <Link to="/">Home</Link>
-      <Link to="/marketplace">Marketplace</Link>
+    <div className="flex justify-between">
+      <div className="left-nav font-bold">verdex</div>
+      <div className="right-nav ">
+        <Link to="/">Home</Link>
+        <Link to="/marketplace">Marketplace</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/register">signup</Link>
+      </div>
     </div>
   );
 };
