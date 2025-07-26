112b89cabc39df95530b6d6979c99640e28503d4
Hema-Nath-Reddy <164506740+Hema-Nath-Reddy@users.noreply.github.com>
2025-07-26 11:54:41 +0530

    home page updated

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 const NavBar = () => {
   return (
-    <div className="flex justify-between">
-      <div className="left-nav font-bold">verdex</div>
+    <div className="flex justify-between mt-1">
+      <div className="left-nav font-bold ml-5">verdex</div>
       <div className="right-nav ">
-        <Link to="/">Home</Link>
-        <Link to="/marketplace">Marketplace</Link>
-        <Link to="/login">Login</Link>
-        <Link to="/register">signup</Link>
+        <Link to="/" className="mr-10">Home</Link>
+        <Link to="/marketplace" className="mr-10">Marketplace</Link>
+        <Link to="/login" className="mr-10">Login</Link>
+        <Link to="/register" className="mr-10">signup</Link>
       </div>
     </div>
   );
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
     <div>
-      <h1>This is home page</h1>
+      <div className="hero h-100 w-250 m-auto rounded-xl flex flex-col justify-center items-center">
+        <h1 className="text-5xl bold text-center">
+          Empowering a Greener Future through Carbon Trading
+        </h1>
+        <p className="m-10">
+          Verdex is a leading platform for trading carbon credits, connecting
+          Indian businesses committed to sustainability. Our platform offers a
+          secure, transparent, and efficient marketplace for buying and selling
+          carbon credits, helping organizations achieve their environmental
+          goals.
+        </p>
+        <div className="home-btns ">
+          <button className="m-5">Sign up</button>
+          <button>Explore Marketplace</button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
