6a592a54d584afc59b387f407b28596fed018f17
6ft-alien <nityanand.exe@gmail.com>
2025-07-29 11:32:27 +0530

    navbar done

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,14 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
 const NavBar = () => {
   return (
-    <div className="flex justify-between mt-1">
-      <div className="left-nav font-bold ml-5">verdex</div>
+    <div className="w-full flex justify-between items-center h-15 bg-white sticky shadow-lg top-0 z-1000">
+      <div className="left-nav font-bold ml-7.5 text-2xl">
+        <Link to="/">VerdeX</Link>
+      </div>
       <div className="right-nav ">
-        <Link to="/" className="mr-10">Home</Link>
-        <Link to="/marketplace" className="mr-10">Marketplace</Link>
-        <Link to="/login" className="mr-10">Login</Link>
-        <Link to="/register" className="mr-10">signup</Link>
+        <Link
+          to="/"
+          className="font-semibold mr-10 text-lg hover:text-[#098409] hover:bg-[#0000001A] rounded-4xl px-4 py-2 transition-all duration-100"
+        >
+          Homepage
+        </Link>
+        <Link
+          to="/marketplace"
+          className="font-semibold mr-10 text-lg hover:text-[#098409] hover:bg-[#0000001A] rounded-4xl px-4 py-2 transition-all duration-100"
+        >
+          Marketplace
+        </Link>
+        <Link
+          to="/login"
+          className="font-semibold mr-10 text-lg hover:text-[#098409] hover:bg-[#0000001A] rounded-4xl px-4 py-2 transition-all duration-100"
+        >
+          About Us
+        </Link>
+        <Link
+          to="/register"
+          className="logbtn font-semibold border-1 hover:text-[#098409] rounded-4xl px-4 py-2 mr-7.5 text-lg transition-all duration-100"
+        >
+          Login/Sign Up
+        </Link>
       </div>
     </div>
   );
